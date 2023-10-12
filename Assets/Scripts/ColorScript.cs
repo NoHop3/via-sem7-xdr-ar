72cd2c8c1b97ef1ffbb1309ef8669b3541763d34
NoHop3 <u_sux@abv.bg>
2023-10-12 12:06:32 +0200

    refactor(scripts): Refactoring scripts.
    Moving all color logic to ColorScript and all to-be-implemented modification logic to Modification script

--- Assets/Scripts/ColorScript.cs
+++ Assets/Scripts/ColorScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteriorScript : MonoBehaviour
+public class ColorScript : MonoBehaviour
 {
     public Color[] colors;
     public Renderer[] mats;
